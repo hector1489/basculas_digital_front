6918e647b13d0d85fc1c6648adafda890ff63eb1
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2025-07-17 15:53:32 -0400

    fix route

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://hector1489.github.io/basculas_digital_front',
+  base: 'https://hector1489.github.io/basculas_digital_front/',
 })
